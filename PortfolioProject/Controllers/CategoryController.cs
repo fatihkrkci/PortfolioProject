90a94d4d2a0b75dc6ecb0b78f476d21cef3f4262
Fatih Kürekçi <fatihkr02@gmail.com>
2024-09-10 21:41:04 +0300

    added Admin Template

---
+++ PortfolioProject/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public ActionResult CategoryList()
+        {
+            return View();
+        }
+
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+    }
+}
