3df01e8f6f9f5636edef5552db985bfa954b8c1c
Fatih Kürekçi <fatihkr02@gmail.com>
2024-09-26 21:58:13 +0300

    completed ExperienceController & updated MessageController & created StatisticsController

---
+++ PortfolioProject/Controllers/ExperienceController.cs
@@ -0,0 +1,60 @@
+﻿using PortfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
+
+        public ActionResult ExperienceList()
+        {
+            var values = context.Experience.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateExperience(Experience experience)
+        {
+            context.Experience.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public ActionResult DeleteExperience(int id)
+        {
+            var value = context.Experience.Find(id);
+            context.Experience.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateExperience(int id)
+        {
+            var value = context.Experience.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateExperience(Experience experience)
+        {
+            var value = context.Experience.Find(experience.ExperienceID);
+            value.Title = experience.Title;
+            value.SubTitle = experience.SubTitle;
+            value.Description = experience.Description;
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+    }
+}
--- PortfolioProject/Controllers/MessageController.cs
+++ PortfolioProject/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PortfolioProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,28 @@ namespace PortfolioProject.Controllers
 {
     public class MessageController : Controller
     {
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
+
         public ActionResult Inbox()
         {
-            return View();
+            var values = context.Contact.ToList();
+            return View(values);
+        }
+
+        public ActionResult ChangeMessageStatusToTrue(int id)
+        {
+            var value = context.Contact.Find(id);
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public ActionResult ChangeMessageStatusToFalse(int id)
+        {
+            var value = context.Contact.Find(id);
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
         }
     }
 }
